1f5837388ac03a8f0a3fe1acb25fa56cad7faf6f
Aymeric Trouillard <cashayme72@gmail.com>
2019-09-19 13:56:04 +0200

    Ajout du sytème d'inscription

---
+++ application/controllers/registration.php
@@ -0,0 +1,51 @@
+<?php
+  
+   class Registration extends CI_Controller {
+   
+    public function __construct() { 
+        parent::__construct(); 
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('bcrypt');
+        $this->load->library('layout');
+         
+    } 
+	
+    public function index() {			
+
+    $this->load->library('form_validation');
+			
+	 /* Validation rule */
+    $this->form_validation->set_rules('name', 'Nom', 'required');
+    $this->form_validation->set_rules('first_name', 'Prénom', 'required');
+    $this->form_validation->set_rules('alias', 'Nom de compte', 'required');
+    $this->form_validation->set_rules('birth_date', 'Date de naissance', 'required');
+    $this->form_validation->set_rules('sex', 'Sexe', 'required');
+	$this->form_validation->set_rules('email', 'Email', 'required|valid_email|callback_check_customer');
+    $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]|max_length[35]');
+    $this->form_validation->set_rules('city_address', 'Ville', 'required');
+    $this->form_validation->set_rules('zip_code_address', 'Code postal', 'required');
+    $this->form_validation->set_rules('address', 'Adresse', 'required');	 
+			
+        if ($this->form_validation->run() == FALSE) { 
+            $this->layout->view('inscription'); 
+        } 
+        else { 
+            $this->load->model('register_model');
+		    $this->register_model->saveCustomer();
+		    $success = "Vous êtes enregistré !";
+            $this->layout->view('inscription', compact('success')); 
+        } 
+    }
+
+	public function check_customer($email)
+	{
+	    $query = $this->db->where('email', $email)->get("user");
+		if ($query->num_rows() > 0)
+		{
+			$this->form_validation->set_message('check_customer','L\'email '.$email.' est déjà utilisé par un autre compte');
+		    return FALSE;
+		}
+		else 
+			return TRUE;
+	  }	
+   }
